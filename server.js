29f3545895e61192e3e1aaf5979b4e6bbebfcd39
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-06-14 00:19:25 +0800

    server port

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.get('/ibm', (req, res) => {
 })
 
 /* -------------------- Run Server -------------------- */
-const port = process.env.PORT || 4001;
+const port = process.env.PORT || 5000;
 if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
