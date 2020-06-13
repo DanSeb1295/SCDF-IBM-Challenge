3c0a965ac1a211a6f73871654a9a3d16e8850dfb
DanSeb1295 <daniel-sebastian95@hotmail.com>
2020-06-13 23:40:06 +0800

    added redspot > 0.5 weight. done allocation UI

--- server.js
+++ server.js
@@ -5,14 +5,6 @@ const app = express();
 app.use(express.json({limit: '50mb', extended: true})); // support json encoded bodies
 app.use(express.urlencoded({limit: '50mb', extended: true}));
 
-// ENV Var
-DATABASE="BLUDB"
-HOSTNAME="dashdb-txn-sbox-yp-dal09-11.services.dal.bluemix.net"
-PORT=50000
-UID="zft48485"
-PWD="pg52md3zdt^4hm83"
-TABLE="ZFT48485.FIRERISK"
-
 const connStr = "DATABASE=" + DATABASE + ";HOSTNAME=" + HOSTNAME + ";UID=" + UID + ";PWD=" + PWD + ";PORT=" + PORT + ";PROTOCOL=TCPIP";
 
 /* -------------------- APIs -------------------- */
